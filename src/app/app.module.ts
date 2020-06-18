9b64f3a714e72c611598a261932c708d64f0675d
edithntinyari <emothegi@gmail.com>
2020-06-18 15:09:40 +0300

    added search-form.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { UpperCasePipe } from './upper-case.pipe';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RepositoriesComponent,
-    UpperCasePipe
+    UpperCasePipe,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
