cade40ae5791fc491cde4c6a0050eb7c169e156f
edithntinyari <emothegi@gmail.com>
2020-06-19 15:35:50 +0300

    added changes in repository.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SearchRequestService} from './search-request.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
-import { UpperCasePipe } from './upper-case.pipe';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { UserInfoComponent } from './user-info/user-info.component';
+import { RouterModule } from '@angular/router';
+import { RoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UpperCasePipe } from './upper-case.pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RepositoriesComponent,
-    UpperCasePipe,
     SearchFormComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    UpperCasePipe
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RoutingModule,
+    RouterModule,
+    FormsModule,
+  
+    HttpClientModule,
+  
+
   ],
-  providers: [],
+  providers: [SearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
